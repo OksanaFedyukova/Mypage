27460485b3c143ba21b8c55d0ba988a3ae40011b
Oksana FEDYUKOVA <avseniya22@gmail.com>
2023-12-30 14:04:11 +0100

    +

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl =  'mongodb://localhost:27017/portafolio';
-//const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/portafolio';
+//const dbUrl =  'mongodb://localhost:27017/portafolio';
+const dbUrl = process.env.DB_URL;
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
