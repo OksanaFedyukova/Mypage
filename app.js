3a69b262efeb6435217212af16117f0f465f16ca
Oksana FEDYUKOVA <avseniya22@gmail.com>
2024-01-05 16:27:32 +0100

    +

--- app.js
+++ app.js
@@ -175,7 +175,9 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
+const port = 3000;
+
 app.listen(port, () => {
     console.log(`Serving on port ${port}`)
 })
